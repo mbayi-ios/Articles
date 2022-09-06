66c013002f3214bf4d113b77f11e47e711eb806b
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-06 13:21:15 +0300

    Initial Commit

---
+++ Articles/ArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesApp.swift
+//  Articles
+//
+//  Created by Amby on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
