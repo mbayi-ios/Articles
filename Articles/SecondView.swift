a5bbf9a87fbaa25cd42caa938bc2719bfd5dbb93
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-06 13:57:11 +0300

    first commit

---
+++ Articles/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  Articles
+//
+//  Created by Amby on 06/09/2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
---
+++ Articles/Writer.swift
@@ -0,0 +1,8 @@
+//
+//  Writer.swift
+//  Articles
+//
+//  Created by Amby on 06/09/2022.
+//
+
+import Foundation
